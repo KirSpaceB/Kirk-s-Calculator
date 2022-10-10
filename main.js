b2e191f60512b4b53b88f108206fbf2b43183e55
KirSpaceB <productivekirk@gmail.com>
2022-10-10 07:17:05 -0700

    -Create the grids for the buttons

---
+++ main.js
@@ -0,0 +1,11 @@
+//add audio and click effects to the calaculator
+// add background theme to calculator most likilt science and mathmatic based
+
+//HTML/CSS/JS
+//JS
+
+//Have views related code in views folder 
+//Have model and controll related code in code folder
+
+
+//makes sure unit test you write cover everything fuctions do, mutltiple input = multiple outputs
