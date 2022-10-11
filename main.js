71187f98ae50277e41e1771364c19001899096bb
KirSpaceB <productivekirk@gmail.com>
2022-10-11 06:51:17 -0700

    main.css main.html modified.js

--- main.js
+++ main.js
@@ -1,11 +1,64 @@
-//add audio and click effects to the calaculator
-// add background theme to calculator most likilt science and mathmatic based
+var calcScreen = document.querySelector('.calculator-screen');
+var numOne = document.querySelector('.one');
+var numTwo = document.querySelector('.two');
+var numThree = document.querySelector('.three');
+var numFour = document.querySelector('.four');
+var numFive = document.querySelector('.five');
+var numSix = document.querySelector('.six');
+var numSeven = document.querySelector('.seven');
+var numEight = document.querySelector('.eight')
+var numNine = document.querySelector('.nine');
+var addition = document.querySelector('.addition');
+var subtract = document.querySelector('.subtract');
+var multiply = document.querySelector('.multiply');
+var divide = document.querySelector('.divide');
+var equal = document.querySelector('.equalsign');
 
-//HTML/CSS/JS
-//JS
+//Giving the numbers clickability
+//The functions searches the doc finds the id of the specific tag that has calc-screen then .value gets the integer value of 'calc-screen' then add numOne value to it.
+function onClickButton1() {
+    document.getElementById('calc-screen').value += numOne.value;
+}
+function onClickButton2() {
+    document.getElementById('calc-screen').value += numTwo.value;
+}
+function onClickButton3() {
+    document.getElementById('calc-screen').value += numThree.value;
+}
+function onClickButton4() {
+    document.getElementById('calc-screen').value += numFour.value;
+}
+function onClickButton5() {
+    document.getElementById('calc-screen').value += numFive.value
+}
+function onClickButton6() {
+    document.getElementById('calc-screen').value += numSix.value;
+}
+function onClickButton7() {
+    document.getElementById('calc-screen').value += numSeven.value;
+}
+function onClickButton8() {
+    document.getElementById('calc-screen').value += numEight.value;
+}
+function onClickButton9() {
+    document.getElementById('calc-screen').value += numNine.value;
+}
 
-//Have views related code in views folder 
-//Have model and controll related code in code folder
+//Giving operators clickability
+function addOnClick() {
+    document.getElementById('calc-screen').value += addition.value;
+}
+function subOnClick() {
+    document.getElementById('calc-screen').value += subtract.value;
+}
+function multiplyOnClick() {
+    document.getElementById('calc-screen').value += multiply.value;
+}
+function divideOnClick() {
+    document.getElementById('calc-screen').value += divide.value;
+}
 
-
-//makes sure unit test you write cover everything fuctions do, mutltiple input = multiple outputs
+operate(num1,num2,+) {
+    
+}
+// How can we do an hold a number while calling the operational to add another number
