e1807a80007c921916c7c98168d7c899d755b28f
KirSpaceB <productivekirk@gmail.com>
2022-10-13 13:38:26 -0700

    main.css main.html main.js

--- main.js
+++ main.js
@@ -1,70 +1,34 @@
-var calcScreen = document.querySelector('.calculator-screen');
-var numOne = document.querySelector('.one');
-var numTwo = document.querySelector('.two');
-var numThree = document.querySelector('.three');
-var numFour = document.querySelector('.four');
-var numFive = document.querySelector('.five');
-var numSix = document.querySelector('.six');
-var numSeven = document.querySelector('.seven');
-var numEight = document.querySelector('.eight')
-var numNine = document.querySelector('.nine');
-var addition = document.querySelector('.addition');
-var subtract = document.querySelector('.subtract');
-var multiply = document.querySelector('.multiply');
-var divide = document.querySelector('.divide');
-var equal = document.querySelector('.equalsign');
+var numBtn = document.querySelectorAll('.numberBtn');
+var opBtn = document.querySelectorAll('.operatorBtn')
+var screen1 = document.getElementById('currentOperationScreen');
+var screen2 = document.getElementById('previousScreenOperation');
 
-const calcNumbers = [1,2,3,4,5,6,7,8,9];
+var inputOne = " ";
 
-//Giving the numbers clickability
-//The functions searches the doc finds the id of the specific tag that has calc-screen then .value gets the integer value of 'calc-screen' then add numOne value to it.
-function onClickButton1() {
-    document.getElementById('calc-screen').value += numOne.value;
-}
-function onClickButton2() {
-    document.getElementById('calc-screen').value += numTwo.value;
-}
-function onClickButton3() {
-    document.getElementById('calc-screen').value += numThree.value;
-}
-function onClickButton4() {
-    document.getElementById('calc-screen').value += numFour.value;
-}
-function onClickButton5() {
-    document.getElementById('calc-screen').value += numFive.value
-}
-function onClickButton6() {
-    document.getElementById('calc-screen').value += numSix.value;
-}
-function onClickButton7() {
-    document.getElementById('calc-screen').value += numSeven.value;
-}
-function onClickButton8() {
-    document.getElementById('calc-screen').value += numEight.value;
-}
-function onClickButton9() {
-    document.getElementById('calc-screen').value += numNine.value;
+
+
+function appendNum(number) {
+    parseInt(number)
+    screen1.textContent += number;
+    inputOne = number
 }
 
-//Giving operators clickability
-function addOnClick() {
-    document.getElementById('calc-screen').value += addition.value;
-}
-function subOnClick() {
-    document.getElementById('calc-screen').value += subtract.value;
-}
-function multiplyOnClick() {
-    document.getElementById('calc-screen').value += multiply.value;
-}
-function divideOnClick() {
-    document.getElementById('calc-screen').value += divide.value;
+function appendOp(operator) {
+    screen1.textContent += operator;
 }
 
-function operate() {
-    const initalValue = 0;
-    const sum = calcNumbers.reduce(
-        (previousVal, currentVal) => previousVal + currentVal, initalValue
-    )
-    console.log(sum)
+function moveToScreen2() {
+
 }
-// How can we do an hold a number while calling the operational to add another number
+
+opBtn.forEach((operator) =>
+    operator.addEventListener('click', () => appendOp(operator.textContent)),
+)
+
+numBtn.forEach((button) => 
+    button.addEventListener('click', () => appendNum(button.textContent))
+)
+
+//parse the values to integers, after add them together and save to new variable, then covert variable num.toString, 
+//then set the inner HTML to the bottom div first, then add the one thats set to the top, make sure to make both divs innerHTML. 
+//Remeber to covert variables back and fourth
